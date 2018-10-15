b5798a51eac89aa15dcedaeda95db73d1ad44418
Nick Espig <nickespig@gmail.com>
2018-10-15 15:26:46 +0200

    Open FaqBlock if it is reloaded (eg. image position change)

--- ftw/faqblock/browser/faqblock.py
+++ ftw/faqblock/browser/faqblock.py
@@ -4,3 +4,6 @@ from ftw.simplelayout.contenttypes.browser.textblock import TextBlockView
 
 class FaqBlockView(TextBlockView):
     template = ViewPageTemplateFile('faqblock.pt')
+
+    def is_reload(self):
+        return self.request.URL.endswith('sl-ajax-reload-block-view')
