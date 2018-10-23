7e09d570e2b3890d30888a07399ca24fe0b6b163
Nick Espig <nickespig@gmail.com>
2018-10-23 13:29:13 +0200

    Title must be required, what is a faq without questions
    Because of the title is required we do not need the show_title anymore and so we omit it.

--- ftw/faqblock/contents/faqblock.py
+++ ftw/faqblock/contents/faqblock.py
@@ -4,6 +4,9 @@ from ftw.simplelayout.contenttypes.contents.textblock import TextBlock
 from ftw.simplelayout.contenttypes.contents.textblock import TextBlockActions
 from ftw.simplelayout.contenttypes.contents.textblock import TextBlockModifier
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.directives import form
+from zope import schema
+from zope.i18nmessageid import Message
 from zope.interface import alsoProvides
 from zope.interface import implements
 
@@ -11,6 +14,15 @@ from zope.interface import implements
 class IFaqBlockSchema(ITextBlockSchema):
     """Faq block for simplelayout
     """
+
+    title = schema.TextLine(
+        title=Message(u'label_title',
+                      domain='ftw.simplelayout',
+                      default=u'Title'),
+        required=True)
+
+    form.omitted('show_title')
+    form.order_before(title='*')
 
 
 alsoProvides(IFaqBlockSchema, IFormFieldProvider)
